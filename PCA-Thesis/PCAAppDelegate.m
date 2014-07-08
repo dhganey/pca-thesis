ff7dbf566abf3e21ed2b50b368b70d318a2ac2cc
David Ganey <dhganey@gmail.com>
2014-07-07 21:06:45 -0700

    Add Parse framework

--- PCA-Thesis/PCAAppDelegate.m
+++ PCA-Thesis/PCAAppDelegate.m
@@ -8,11 +8,15 @@
 
 #import "PCAAppDelegate.h"
 
+#import <Parse/Parse.h>
+
 @implementation PCAAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    [Parse setApplicationId:@"2aL0TJfXa5hZihCMG18LG7lqINJP6HMpoNc1mZAm"
+                  clientKey:@"bSuKFhNwuIM0CPzybJI4sjTyioVfWcAXHfCVzBDf"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 							
