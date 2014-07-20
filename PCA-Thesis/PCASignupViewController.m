57303b518e280e022b7deddf8f21fd478674389a
David Ganey <dhganey@gmail.com>
2014-07-20 14:56:44 -0700

    Remove old scroll view code

--- PCA-Thesis/PCASignupViewController.m
+++ PCA-Thesis/PCASignupViewController.m
@@ -28,19 +28,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    //add observers for notifications
-    [[NSNotificationCenter defaultCenter]
-     addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:self.view.window];
-    
-    [[NSNotificationCenter defaultCenter]
-     addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:self.view.window];
-    
-    //set size of scrollView
-    CGSize scrollContentSize = CGSizeMake(320, 400);
-    self.scrollView.contentSize = scrollContentSize;
-    
-    //[self.view endEditing:YES];
     
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
     tapGesture.cancelsTouchesInView = NO;
@@ -63,36 +50,6 @@
     [self.phoneField resignFirstResponder];
     
     [self.view endEditing:YES];
-}
-
-//Notification method to scroll when keyboard appears
--(void)keyboardWillShow:(NSNotification *) notification
-{
-    NSDictionary *userInfo = notification.userInfo; //save userInfo from notification
-    CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-    CGRect viewFrame = self.scrollView.frame;
-    viewFrame.size.height -= (keyboardSize.height + 10); //shift up by size of keyboard
-    self.scrollView.frame = viewFrame;
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationBeginsFromCurrentState:YES];
-    [UIView setAnimationDuration:.3];
-    [self.scrollView setFrame:viewFrame];
-    [UIView commitAnimations];
-}
-
-//Notification method to move scroll area when keyboard hides
--(void) keyboardWillHide:(NSNotification *) notification
-{
-    NSDictionary *userInfo = notification.userInfo; //save userInfo from notification
-    CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
-    CGRect viewFrame = self.scrollView.frame;
-    viewFrame.size.height += (keyboardSize.height + 10); //shift down by size of keyboard
-    
-    [UIView beginAnimations:nil context:NULL];
-    [UIView setAnimationBeginsFromCurrentState:YES];
-    [UIView setAnimationDuration:.3];
-    [self.scrollView setFrame:viewFrame];
-    [UIView commitAnimations];
 }
 
 //Hides the keyboard. Called by the gesture recognizer in viewDidLoad
